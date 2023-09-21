3675fed67fb54f843b5858e18cebfc20be7a50d8
b3njah <benjahjp@gmail.com>
2023-09-21 18:18:49 +0300

    twin

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 fn main(){
-    let result = palindrome("level");
-    println!("{:?}", result);
+    println!("{:?}", twin("level", "level"));
 }
+  
+fn twin(first_word: &str, second_word: &str) -> bool{
+    let mut sorted_first: Vec<char> = first_word.chars().collect();
+    sorted_first.sort();
 
-fn palindrome(word: &str) -> bool{
-    let characters: String = word.chars().rev().collect();
+    let mut sorted_second: Vec<char> = second_word.chars().collect();
+    sorted_second.sort();
 
-    if word == characters{
-        true
-    } else {
-        false
-    }
+    sorted_first == sorted_second
 }
--- src/twin_words.rs
+++ src/twin_words.rs
@@ -1,3 +1,7 @@
+fn main(){
+    println!("{:?}", twin("level", "level"));
+}
+  
 fn twin(first_word: &str, second_word: &str) -> bool{
     let mut sorted_first: Vec<char> = first_word.chars().collect();
     sorted_first.sort();
@@ -5,9 +9,5 @@ fn twin(first_word: &str, second_word: &str) -> bool{
     let mut sorted_second: Vec<char> = second_word.chars().collect();
     sorted_second.sort();
 
-    if sorted_first == sorted_second {
-        true
-    }
-
-    false
+    sorted_first == sorted_second
 }
